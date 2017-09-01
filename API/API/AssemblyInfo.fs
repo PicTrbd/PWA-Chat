3709feeba03a0ed2748d26e7d321e639cbe6de36
Monnier <paul.monnier@epitech.eu>
2017-09-01 14:54:33 +0200

    Project structure done

---
+++ API/API/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace API.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("API")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("API")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("0ed08dd9-0224-47a9-9f50-b8eab71ebe04")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ API/API/Models/MessageModel.fs
@@ -0,0 +1,32 @@
+﻿namespace API.MessageModel
+
+open System
+open System.Collections.Concurrent
+
+
+type Message = {
+    Id: Guid
+    Date: DateTime
+    Sender: string 
+    Message: string
+}
+
+type MessageRepository = {
+    Add : Message -> Option<Message>
+    GetAll: unit -> seq<Message>    
+}
+
+
+module MessageRepositoryDb = 
+
+    let private messages = new ConcurrentDictionary<Guid, Message>()
+
+    let add msg = 
+        let msgCopy = { msg with Id = Guid.NewGuid() }
+        if messages.TryAdd(msgCopy.Id, msgCopy) then
+            Some(msgCopy)
+        else
+            None
+
+    let getAll () = 
+        messages.Values |> Seq.cast
