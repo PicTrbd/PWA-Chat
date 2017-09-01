3f79f9f5cb43c6f7d6c4aae530245e39734582c8
Thomas Raballand <t.raballand@gmail.com>
2017-09-01 16:07:34 +0200

    [App] Css modifications

--- PWA-App/scripts/main.js
+++ PWA-App/scripts/main.js
@@ -17,6 +17,7 @@
  *
  */
 /* eslint-env browser */
+
 (function() {
   'use strict';
 
@@ -37,32 +38,18 @@
       (window.location.protocol === 'https:' || isLocalhost)) {
     navigator.serviceWorker.register('service-worker.js')
     .then(function(registration) {
-      // updatefound is fired if service-worker.js changes.
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
       registration.onupdatefound = function() {
-        // updatefound is also fired the very first time the SW is installed,
-        // and there's no need to prompt for a reload at that point.
-        // So check here to see if the page is already controlled,
-        // i.e. whether there's an existing service worker.
         if (navigator.serviceWorker.controller) {
-          // The updatefound event implies that registration.installing is set:
-          // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
           var installingWorker = registration.installing;
-
           installingWorker.onstatechange = function() {
             switch (installingWorker.state) {
               case 'installed':
-                // At this point, the old content will have been purged and the
-                // fresh content will have been added to the cache.
-                // It's the perfect time to display a "New content is
-                // available; please refresh." message in the page's interface.
                 break;
-
               case 'redundant':
                 throw new Error('The installing ' +
                                 'service worker became redundant.');
-
               default:
-                // Ignore
             }
           };
         }
@@ -71,6 +58,4 @@
       console.error('Error during service worker registration:', e);
     });
   }
-
-  // Your custom JavaScript goes here
 })();
