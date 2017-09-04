38dd31c98766210e7e5c47087a46fd6d1cf46425
Thomas Raballand <t.raballand@gmail.com>
2017-09-04 12:01:48 +0200

    Old service worker

--- pwa-app/src/index.js
+++ pwa-app/src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js')
-    .then(function(registration) {
-      console.log('Registration successful, scope is:', registration.scope);
-    })
-    .catch(function(error) {
-      console.log('Service worker registration failed, error:', error);
-    });
-  }
+registerServiceWorker();
