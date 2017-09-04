c4b912be53626e40ef36cfbff1a28efe0e93530b
Monnier <paul.monnier@epitech.eu>
2017-09-04 17:29:59 +0200

    Service Worker modfied, working well on device

--- pwa-app/src/index.js
+++ pwa-app/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js');
+  }
