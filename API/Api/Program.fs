0608ed39f7f17d1dae25c7724ae2ba569c573405
Monnier <paul.monnier@epitech.eu>
2017-09-05 10:10:34 +0200

    Api project added

---
+++ API/Api/Program.fs
@@ -0,0 +1,10 @@
+﻿open Api.Model
+open Suave.Web
+open Api.Controller
+open MessageController
+open MessageRepositoryDb
+
+[<EntryPoint>]
+let main argv =
+  startWebServer defaultConfig (messageController messageRepositoryDb)
+  0
