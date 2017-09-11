1d0de107fcd2b06e4b2970892bc5b0b3f64ef9d1
Monnier <paul.monnier@epitech.eu>
2017-09-11 15:32:49 +0200

    New Api using Signal R + Web project used to test the new Api

---
+++ API/Api-v2/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+
+namespace Api_v2
+{
+    public class ChatHub : Hub
+    {
+       
+        public void SendMessage(string json)
+        {
+            try
+            {
+                var formatedMsg = Dependencies.ChatController.AddMessage(json);
+                Clients.All.addMessage(JsonConvert.SerializeObject(formatedMsg));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error on SendMessage => {0}", e);
+            }
+        }
+    }
+}
---
+++ API/Api-v2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+
+[assembly: OwinStartup(typeof(Api_v2.Startup))]
+namespace Api_v2
+{
+    //.Net Core 2.0 Cela ne marche pas, WebApp est null lorsqu'on lance le programme
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dependencies.ChatController = new ChatController();
+
+            string url = "http://localhost:8080";
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine("Server running on {0}", url);
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR("/chat", new HubConfiguration());
+        }
+    }
+
+
+}
