075e40c281c46abbe3907fc2b656156085ae4a1a
Monnier <paul.monnier@epitech.eu>
2017-10-05 13:22:47 +0200

    webpack config

---
+++ PWA-App/webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'build');
+var APP_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      { test: /\.png$/, loader: "url-loader?mimetype=image/png" },      
+      { test: /\.css$/, loader: "style-loader!css-loader" },      
+      { test : /\.js?/, include : APP_DIR, loader : 'babel-loader' }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+        { from: './public/index.html', to: BUILD_DIR },
+        { from: './public/manifest.json', to: BUILD_DIR },
+        { from: './public/sw.js', to: BUILD_DIR },
+        { from: './public/images/*', to: BUILD_DIR }        
+    ])]
+};
+
+module.exports = config;
