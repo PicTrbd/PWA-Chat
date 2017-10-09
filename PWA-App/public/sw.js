295a623f241c12110dc62301c53d271458096ada
Thomas Raballand <t.raballand@gmail.com>
2017-10-09 15:34:33 +0200

    Fix web push notifications serviceworker

--- PWA-App/public/sw.js
+++ PWA-App/public/sw.js
@@ -5,11 +5,17 @@ self.addEventListener('notificationclose', event => {
 });
 
 self.addEventListener('push', event => {
-  var payload = event.data;
-  if (event.data.From !== undefined) {
+  var payload = "";
+  try {
+    payload = event.data.json();    
+  } catch (error) {
+    console.log("Error parsing the JSON");
+  }
+
+  if (payload.From !== undefined) {
     var options = {
       body: "\nFrom : "
-            .concat(payload.From),         
+            .concat(payload.From.substring(0, 8)),         
       icon: "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678092-sign-add-128.png",
     }
     event.waitUntil(self.registration.showNotification("You have a new message !", options));
@@ -19,6 +25,6 @@ self.addEventListener('push', event => {
       body: "This is a test notification",         
       icon: "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678092-sign-add-128.png",
     }
-    event.waitUntil(self.registration.showNotification("Test notification !", options));    
+    event.waitUntil(self.registration.showNotification("Test notification !!!", options));    
   }
 });
